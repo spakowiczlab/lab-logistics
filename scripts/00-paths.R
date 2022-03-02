d5c1ed13e5fa2cf1c56b8d9d67c84a5ece2f0904
dspak <daniel.spakowicz@gmail.com>
2022-03-02 10:26:02 -0500

    make training table

---
+++ scripts/00-paths.R
@@ -0,0 +1,15 @@
+###############################
+### Locate the $HOME directory
+home <- Sys.getenv(x = "R_USER", unset = NA)
+if (is.na(home)) stop("Cannot find 'HOME' from environment variable s.")
+
+### Find the JSON path information in the appropriate directory.
+jinfo <- file.path("C:","Users", "spak01", "Documents", "GitHub", "lab-logistics","lab-logistics.json")
+if (!file.exists(jinfo)) stop("Cannot locate file: '", jinfo, "'.\n", sep='')
+## parse it
+library(rjson)
+temp <- fromJSON(file = jinfo)
+paths <- temp$paths
+detach("package:rjson")
+### clean up
+rm(home, jinfo, temp)
